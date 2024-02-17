a0a5e1f3f35649c212b6383a5c190445913576c7
Artyom Eroshkin <artemeroshkin47@gmail.com>
2024-02-17 09:41:13 +0300

    add notifier

--- go.mod
+++ go.mod
@@ -8,4 +8,12 @@ require (
 	github.com/jmoiron/sqlx v1.3.5
 )
 
-require github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
+	github.com/go-shiori/dom v0.0.0-20230515143342-73569d674e1c // indirect
+	github.com/go-shiori/go-readability v0.0.0-20240204090920-819593fddc6b // indirect
+	github.com/gogs/chardet v0.0.0-20211120154057-b7413eaefb8f // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- internal/notifier/notifier.go
+++ internal/notifier/notifier.go
@@ -2,15 +2,24 @@ package notifier
 
 import (
 	"context"
+	"fmt"
+	"github.com/go-shiori/go-readability"
+	"io"
+	"net/http"
 	"news_ai_feed/internal/model"
+	"regexp"
+	"strings"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+//TODO: Сделать транзакции
+//TODO: Заврапить ошибки
+
 type ArticleProvider interface {
 	AllNotPosted(ctx context.Context, since time.Time, limit uint64) ([]model.Article, error)
-	MarkPosted(ctx context.Context, id int64)
+	MarkPosted(ctx context.Context, id int64) error
 }
 
 type Summarizer interface {
@@ -38,5 +47,73 @@ func New(articles ArticleProvider, summarizer Summarizer, bot *tgbotapi.BotAPI, 
 }
 
 func (n *Notifier) SelectAndSend(ctx context.Context) error {
+	topOneArticles, err := n.articles.AllNotPosted(ctx, time.Now().Add(-n.lookupTimeWindow), 1)
+	if err != nil {
+		return err
+	}
 
+	if len(topOneArticles) == 0 {
+		return nil
+	}
+
+	article := topOneArticles[0]
+
+	//begin
+
+	summary, err := n.extractSummary(ctx, article)
+	if err != nil {
+		return err
+	}
+
+	if err := n.sendArticle(article, summary); err != nil {
+		return err
+	}
+
+	//commit
+	return n.articles.MarkPosted(ctx, article.ID)
+}
+
+func (n *Notifier) extractSummary(ctx context.Context, article model.Article) (string, error) {
+	var r io.Reader
+
+	if article.Summary != "" {
+		r = strings.NewReader(article.Link)
+	} else {
+		resp, err := http.Get(article.Link)
+		if err != nil {
+			return "", err
+		}
+		defer resp.Body.Close()
+
+		r = resp.Body
+	}
+
+	doc, err := readability.FromReader(r, nil)
+	if err != nil {
+		return "", err
+	}
+
+	summary, err := n.summarizer.Summarize(ctx, cleanText(doc.TextContent))
+
+	return "\n\n" + summary, err
+}
+
+func (n *Notifier) sendArticle(article model.Article, summary string) error {
+	const msgFormat = "*%s*%s\n\n%s"
+
+	msg := tgbotapi.NewMessage(n.channelID, fmt.Sprint(msgFormat, article.Title, summary, article.Link))
+	msg.ParseMode = tgbotapi.ModeMarkdownV2
+
+	_, err := n.bot.Send(msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var redundantNewLines = regexp.MustCompile(`\n{3,}`)
+
+func cleanText(text string) string {
+	return redundantNewLines.ReplaceAllString(text, "\n")
 }
